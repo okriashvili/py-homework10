ff72c8a8ab19f7d0e70816c0d8f65aa88c49229d
mahoo <146084234+okriashvili@users.noreply.github.com>
2025-06-27 21:26:50 +0400

    done 3rg task as well, homework10 done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # python 10th homework!!!!!!!!!!!!!!
-
+from functools import reduce
 
 #დავალება1: გამოიყენეთlambda ფუნქცია sorted() ფუნქციაში, იმისათვის რომ დაასორტიროს მოცემული
 # ლისტი: [(1, 3), (4, 2), (2, 5)] - მასში არსებული ელემენტების მეორე ელემენტის მიხედვით
@@ -10,33 +10,33 @@ print(new_lst)
 
 
 
-#
-# # დავალება2:  დაწერეთ ფუნქცია, რომელიც მომხმარებელს შეაყვანინებს ორ რიცხვს და პირველ რიცხვს გაყოფს მეორე რიცხვზე და დააბრუნებს შედეგს,
-# # დაიჭირეთ ორი ერორი: ის რომ მომხმარებელმა ინტეჯერები შეიყვანოს და ნულზე რომ არ შეიძლება გაყოფა, თითოეული ერორისთვის გამოუტანეთ
-# # შესაბამისი შეტყობინება. (ორივე ერორი უნდა იყოს შესაბამისი ერორებით დაჭერილი, არ გამოიყენოთ ზოგადი იქსეფშენი)
-# def division(num1, num2):
-#     result = num1 / num2
-#     return result
-#
-# try:
-#     num1 = int(input("Enter a number: "))
-#     num2 = int(input("Enter a number: "))
-#     result = division(num1, num2)
-#
-# except ZeroDivisionError:
-#     print("can't divide by zero")
-#
-# except ValueError:
-#     print("insert only Integers")
-#
-# except Exception as e:
-#     print(e)
-#
-# else:
-#     print(result)
-#
-# finally:
-#     print("end of a program")
+
+# დავალება2:  დაწერეთ ფუნქცია, რომელიც მომხმარებელს შეაყვანინებს ორ რიცხვს და პირველ რიცხვს გაყოფს მეორე რიცხვზე და დააბრუნებს შედეგს,
+# დაიჭირეთ ორი ერორი: ის რომ მომხმარებელმა ინტეჯერები შეიყვანოს და ნულზე რომ არ შეიძლება გაყოფა, თითოეული ერორისთვის გამოუტანეთ
+# შესაბამისი შეტყობინება. (ორივე ერორი უნდა იყოს შესაბამისი ერორებით დაჭერილი, არ გამოიყენოთ ზოგადი იქსეფშენი)
+def division(num1, num2):
+    result = num1 / num2
+    return result
+
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    result = division(num1, num2)
+
+except ZeroDivisionError:
+    print("can't divide by zero")
+
+except ValueError:
+    print("insert only Integers")
+
+except Exception as e:
+    print(e)
+
+else:
+    print(result)
+
+finally:
+    print("end of a program")
 
 
 # დავალება3: მოცემულია პროდუქტების ლისტი:
@@ -54,21 +54,21 @@ products = [
     {"name": "Pad", "price": 10},
 ]
 
+# filter() ფუნქციის გამოყენებით გაფილტრეთ და გამოიტანეთ პროდუქტები, რომლის ფასი ნაკლებია 100-ზე;
+expensive = list(filter(lambda x : x["price"] < 100, products))
+print(expensive)
 
+# map() ფუნქციის გამოყენებით გამოიტანეთ ყველა პროდუქტის სახელი და ფასი
+name_and_price = list(map(lambda x : (x["name"], x["price"]), products))
+print(name_and_price)
 
+# sorted() ფუნქციის გამოყენებით დაასორტირეთ პროდუქტების სია ფასის მიხედვით
+price = list(sorted(products, key=lambda x : x["price"]))
+print(price)
 
-
-
-
-
-
-
-
-
-
-
-
-
+# reduce() ფუნქციის გამოყენებით გამოიტანეთ ყველა პროდუქტის ფასების ჯამი
+total_price = reduce(lambda x, y: x + y["price"], products, 0)
+print(total_price)
 
 
 
@@ -82,4 +82,12 @@ def my_func(n):
      else:
          return n + my_func(n - 1)
 print(my_func(int(input("Enter a number: "))))
+
+
+
+
+
+
+
+
 
